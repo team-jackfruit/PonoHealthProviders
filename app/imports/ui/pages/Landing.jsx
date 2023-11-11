715c3c3bd39bb220304bc263a26557a852cc3a79
Taitusi Vadei <tvadei@hawaii.edu>
2023-11-11 11:34:28 -1000

    Second section, Landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
@@ -12,17 +12,18 @@ const Landing = () => (
       </Col>
     </Row>
     {/* This is the Mission section */}
-    <Row>
-      <Col xs={12} md={8} className="text-center text-md-left">
-        <h1>NEED MEDICAL HELP?</h1>
-        <h2>ANXIOUS BECAUSE YOU&apos;RE UNINSURED OR UNDER-INSURED?</h2>
-        <h3>TRYING TO FIND AFFORDABLE HEALTHCARE?</h3>
-        <p>PonoHealth Providers aims to bridge the gap for the uninsured and under-insured, by enhancing healthcare accessibility, promoting informed decision-making, and fostering a community where quality health services are attainable for all, regardless of their financial circumstances.</p>
+    <Row className="mission-section">
+      <Col xs={12} md={8} className="ps-5 text-lg-start">
+        <h2 className="mission-heading">NEED MEDICAL HELP?</h2>
+        <h2 className="mission-heading">ANXIOUS BECAUSE YOU&apos;RE UNINSURED OR UNDER-INSURED?</h2>
+        <h2 className="mission-heading">TRYING TO FIND AFFORDABLE HEALTHCARE?</h2>
+        <p className="mission-text">PonoHealth Providers aims to bridge the gap for the uninsured and under-insured, by enhancing healthcare accessibility, promoting informed decision-making, and fostering a community where quality health services are attainable for all, regardless of their financial circumstances.</p>
       </Col>
-      <Col xs={12} md={4} className="text-center">
-        {/* Your image/icon here */}
+      <Col xs={12} md={4}>
+        <Image src="images/second_section_landing.png" />
       </Col>
     </Row>
+    {/* This is the Services Provided section */}
   </Container>
 );
 
