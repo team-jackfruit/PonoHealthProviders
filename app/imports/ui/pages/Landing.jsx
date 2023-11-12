e9e691dd15149af7e4c4b7eb4b0b7e698f25b8f7
Taitusi Vadei <tvadei@hawaii.edu>
2023-11-11 18:11:29 -1000

    Done skeleton of third section. Left with styling

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,59 +6,59 @@ const Landing = () => (
   <Container fluid>
     {/* This is the Page Banner */}
     <Row className="justify-content-md-center align-items-center hero-banner">
-      <Col xs={12} className="text-center">
+      <Col className="text-center">
         <h1 className="header-title">Welcome to PonoHealth Providers</h1>
         <h2 className="header-subtitle">Where everyone gets equal access</h2>
       </Col>
     </Row>
     {/* This is the Mission section */}
     <Row className="mission-section">
-      <Col xs={12} md={8} className="ps-5 text-lg-start">
+      <Col className="ps-5 text-lg-start">
         <h2 className="mission-heading">NEED MEDICAL HELP?</h2>
         <h2 className="mission-heading">ANXIOUS BECAUSE YOU&apos;RE UNINSURED OR UNDER-INSURED?</h2>
         <h2 className="mission-heading">TRYING TO FIND AFFORDABLE HEALTHCARE?</h2>
         <p className="mission-text">PonoHealth Providers aims to bridge the gap for the uninsured and under-insured, by enhancing healthcare accessibility, promoting informed decision-making, and fostering a community where quality health services are attainable for all, regardless of their financial circumstances.</p>
       </Col>
-      <Col xs={12} md={4}>
+      <Col>
         <Image src="images/second_section_landing.png" />
       </Col>
     </Row>
     {/* This is the Services Provided section */}
-    <Row>
-      <Col>
-        <h2> Our Services </h2>
+    <Row className="justify-content-center">
+      <Col className="text-center">
+        <h1> Our Services </h1>
       </Col>
     </Row>
-    <Row>
-      <Col>
-        <Image src="images/Find_A_Provider.png" />
+    <Row className="mb-3">
+      <Col className="text-center">
+        <Image src="images/Find_A_Provider.png" width="150" height="150" />
       </Col>
       <Col>
         <h2>Find a Provider</h2>
         <p>We aid you in finding healthcare providers ready to assist you</p>
       </Col>
     </Row>
-    <Row>
-      <Col>
-        <Image src="images/Healthcare_Guide.png" />
+    <Row className="mb-3">
+      <Col className="text-center">
+        <Image src="images/Healthcare_Guide.png" width="150" height="150" />
       </Col>
       <Col>
         <h2>Healthcare Guide</h2>
         <p>Benefit from our collected resources aimed at providing affordable healthcare solutions.</p>
       </Col>
     </Row>
-    <Row>
-      <Col>
-        <Image src="images/Guidance_Support.png" />
+    <Row className="mb-3">
+      <Col className="text-center">
+        <Image src="images/Guidance_Support.png" width="150" height="150" />
       </Col>
       <Col>
         <h2>Guidance Support</h2>
         <p>Our team is ready to answer all your healthcare related queries on our FAQ page.</p>
       </Col>
     </Row>
-    <Row>
-      <Col>
-        <Image src="images/Community_Forum.png" />
+    <Row className="mb-3">
+      <Col className="text-center">
+        <Image src="images/Community_Forum.png" width="150" height="150" />
       </Col>
       <Col>
         <h2>Community Forum</h2>
