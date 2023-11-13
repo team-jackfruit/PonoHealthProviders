33edf6dd5d751ddb1ab9418c18a1348abddb0f94
Taitusi Vadei <tvadei@hawaii.edu>
2023-11-13 01:09:14 -1000

    Completed Landing. Still some clean up but satisfied as of now

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,22 +5,22 @@ import { Container, Row, Col, Image } from 'react-bootstrap';
 const Landing = () => (
   <Container fluid>
     {/* This is the Page Banner */}
-    <Row className="justify-content-md-center align-items-center hero-banner">
+    <Row className="hero-banner">
       <Col className="text-center">
         <h1>Welcome to PonoHealth Providers</h1>
         <h2>Where everyone gets equal access</h2>
       </Col>
     </Row>
     {/* This is the Mission section */}
-    <Row className="mission-section">
-      <Col className="ps-5 text-lg-start">
+    <Row className="mission-section align-content-center">
+      <Col className="col-6 p-5">
         <h2 className="mission-heading">NEED MEDICAL HELP?</h2>
         <h2 className="mission-heading">ANXIOUS BECAUSE YOU&apos;RE UNINSURED OR UNDER-INSURED?</h2>
         <h2 className="mission-heading">TRYING TO FIND AFFORDABLE HEALTHCARE?</h2>
         <p className="mission-text">PonoHealth Providers aims to bridge the gap for the uninsured and under-insured, by enhancing healthcare accessibility, promoting informed decision-making, and fostering a community where quality health services are attainable for all, regardless of their financial circumstances.</p>
       </Col>
-      <Col>
-        <Image src="images/second_section_landing.png" />
+      <Col className="col-4 p-5 text-center">
+        <Image fluid src="images/second_section_landing.png" />
       </Col>
     </Row>
     {/* This is the Services Provided section */}
@@ -29,8 +29,8 @@ const Landing = () => (
         <h1> Our Services </h1>
       </Col>
     </Row>
-    <Row className="mb-3 align-items-center services-section">
-      <Col className="text-center services-section">
+    <Row className="align-items-center services-section">
+      <Col className="text-center services-section p-3">
         <Image src="images/Find_A_Provider.png" width="150" height="150" />
       </Col>
       <Col className="services-section">
@@ -38,7 +38,7 @@ const Landing = () => (
         <p>We aid you in finding healthcare providers ready to assist you</p>
       </Col>
     </Row>
-    <Row className="mb-3 align-items-center services-section">
+    <Row className="align-items-center services-section p-3">
       <Col className="text-center services-section">
         <Image src="images/Healthcare_Guide.png" width="150" height="150" />
       </Col>
@@ -47,7 +47,7 @@ const Landing = () => (
         <p>Benefit from our collected resources aimed at providing affordable healthcare solutions.</p>
       </Col>
     </Row>
-    <Row className="mb-3 align-items-center services-section">
+    <Row className="align-items-center services-section p-3">
       <Col className="text-center services-section">
         <Image src="images/Guidance_Support.png" width="150" height="150" />
       </Col>
@@ -56,7 +56,7 @@ const Landing = () => (
         <p>Our team is ready to answer all your healthcare related queries on our FAQ page.</p>
       </Col>
     </Row>
-    <Row className="mb-3 align-items-center services-section">
+    <Row className="align-items-center services-section p-3">
       <Col className="text-center services-section">
         <Image src="images/Community_Forum.png" width="150" height="150" />
       </Col>
