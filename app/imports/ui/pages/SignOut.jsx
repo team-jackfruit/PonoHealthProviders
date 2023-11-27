d6a2924c111d79915127d1ccb6274a854a5e9d28
Taitusi Vadei <tvadei@hawaii.edu>
2023-11-27 00:15:49 -1000

    Beautified Sign Out Page

--- app/imports/ui/pages/SignOut.jsx
+++ app/imports/ui/pages/SignOut.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col } from 'react-bootstrap';
+import { Col, Container, Carousel, Image } from 'react-bootstrap';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => {
   Meteor.logout();
   return (
-    <Col id="signout-page" className="text-center py-3"><h2>You are signed out.</h2></Col>
+    <Container fluid id="signout-page">
+      <Col md={{ span: 8, offset: 2 }}>
+        <Carousel data-bs-theme="dark">
+          <Carousel.Item interval={5000}>
+            <Image src="images/GoodBye.png" className="w-100" />
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <Image src="images/SignedOut.png" className="w-100" />
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <Image src="images/Enjoyed.png" className="w-100" />
+          </Carousel.Item>
+        </Carousel>
+      </Col>
+    </Container>
   );
 };
 
