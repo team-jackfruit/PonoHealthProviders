4daac60a737e9104a01bd57accbef983ffc5702c
Taitusi Vadei <tvadei@hawaii.edu>
2023-11-28 19:03:15 -1000

    Change-2

--- app/client/main.js
+++
@@ -1,6 +0,0 @@
-// Start the app.
-import '../imports/startup/client/Startup';
-// Import the Bootstrap css.
-import 'bootstrap/dist/css/bootstrap.min.css';
-// Override the default bootstrap styles.
-import './style.css';
