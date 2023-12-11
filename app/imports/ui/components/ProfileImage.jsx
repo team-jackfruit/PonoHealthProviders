0f11d983240138d0dfaa54f1955af02c228dcbe6
Jakapop Khongnawang <jakapop@hawaii.edu>
2023-12-10 20:24:05 -1000

    adjusted image size

--- app/imports/ui/components/ProfileImage.jsx
+++ app/imports/ui/components/ProfileImage.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types'; // Make sure to install prop-types package if you haven't already
+// eslint-disable-next-line no-unused-vars
 import { Button, Card, Image } from 'react-bootstrap';
 import firebase from 'firebase/app';
 import 'firebase/storage';
 
 const ProfileImage = ({ user }) => {
+  // eslint-disable-next-line no-unused-vars
   const [imageURL, setImageURL] = useState(null);
 
+  // eslint-disable-next-line no-unused-vars
   const handleFileUpload = (url) => {
     setImageURL(url);
   };
 
+  // eslint-disable-next-line no-unused-vars
   const uploadImage = (file) => {
     const storageRef = firebase.storage().ref();
     const uploadTask = storageRef.child(`images/${file.name}`).put(file);
@@ -20,12 +24,15 @@ const ProfileImage = ({ user }) => {
     });
   };
   return (
-    <Card className="align-content-center user-card">
+    <Card className="align-content-center user-card" style={{ width: '300px', margin: 'auto' }}>
       <Card.Body className="text-center">
-        <Card.Header className="bold-large-header">Your Profile</Card.Header>
-        <Card className="pb-2"><Image src={user.image} /> </Card>
+        <Card.Header className="bold-large-header">Profile Picture</Card.Header>
+        <Card style={{ width: '100%' }}>
+          <Image src={user.image} style={{ width: '100%', height: 'auto' }} />
+        </Card>
       </Card.Body>
     </Card>
+
   );
 };
 
